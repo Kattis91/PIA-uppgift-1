3f9f9c5d335f86afddd9e592ed688eccc7fb5a84
Kattis91 <katya.svedmark@gmail.com>
2024-09-17 11:35:37 +0200

    Byggt uppgift-1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -7,6 +7,32 @@ import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
+
+    <View style={{ backgroundColor: "#82caff", flex: 1 }}>
+     
+      <View style={{ backgroundColor: "#ff4747", height: 150, justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ textTransform: "uppercase", fontSize: 35 }}>Röd</Text>
+      </View>
+      
+      <View style={{ flexDirection: "row", height: 150, marginTop: 5 }}>
+        <View style={{ backgroundColor: "#50C878", flex: 1 }}>
+          <Text style={{ textTransform: "uppercase", fontSize: 35, paddingLeft: 5 }}>Grön</Text>
+        </View>
+        <View style={{ backgroundColor: "#ffd300", flex: 1, marginLeft: 5 }}></View>
+      </View>
+      
+      <View style={{ flexDirection: "row", height: 75, marginTop: 5 }}>
+        <View style={{ backgroundColor: "#ff4747", flex: 3 }}></View>
+        <View style={{ backgroundColor: "black", flex: 1, marginLeft: 5 }}></View>
+      </View>
+
+      <View style={{ flex: 1 }}></View>
+
+      <View style={{ backgroundColor: "orange", height: 50, justifyContent: "center", alignItems: "flex-end" }}>
+        <Text style={{ textTransform: "uppercase", fontSize: 35, paddingRight: 5 }}>Orange</Text>
+      </View>
+    
+    </View>
     
   );
 }
